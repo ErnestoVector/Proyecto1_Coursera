dda93e9ff05eb5a7a2530b65a598078860ee414e
ErnestoVector <egm.vector@gmail.com>
2017-06-22 13:26:31 -0500

    Initial commit

---
+++ app/src/main/java/ernestovector/proyecto1_coursera/Proyecto1_Coursera.java
@@ -0,0 +1,13 @@
+package ernestovector.proyecto1_coursera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Proyecto1_Coursera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proyecto1__coursera);
+    }
+}
